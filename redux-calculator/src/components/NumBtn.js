4ac599bfe8c2d49aec48da7b37900ca97c497316
Mamoru Kosugiyama <mkhd7930@gmail.com>
2019-12-15 02:07:35 +0900

    電卓作成中

---
+++ redux-calculator/src/components/NumBtn.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NumBtn = ({n}) => {
+    return(
+        <button>{n}</button>
+    );
+}
+
+export default NumBtn;
---
+++ redux-calculator/src/components/PlusBtn.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PlusBtn = () => {
+    return(
+        <button>+</button>
+    );
+}
+
+export default PlusBtn;
